ec86ee9afc0c87ca7d38c6d6497efb245462e9f4
Khamit Amantaev <khamitamantaev@gmail.com>
2021-05-23 11:59:29 +0500

    Добавил кнопочки удаления и обновления и стили

--- simple-todos-react/imports/ui/App.jsx
+++ simple-todos-react/imports/ui/App.jsx
@@ -3,18 +3,44 @@ import { useTracker } from "meteor/react-meteor-data";
 import { TasksCollection } from "/imports/api/TaskCollection";
 import { Task } from "./Task";
 import { TaskForm } from './TaskForm';
+
+const toggleChecked = ({ _id, isChecked }) => {
+  TasksCollection.update(_id, {
+    $set: {
+      isChecked: !isChecked
+    }
+  })
+};
+
+const deleteTask = ({ _id }) => TasksCollection.remove(_id);
+
 export const App = () => {
   const tasks = useTracker(() => TasksCollection.find({}, { sort: { createdAt: -1 } }).fetch());
 
   return (
-    <div>
-      <h1>Welcome to Meteor!</h1>
-      <TaskForm/>
-      <ul>
-        {tasks.map((task) => (
-          <Task key={task._id} task={task} />
-        ))}
-      </ul>
+    <div className="app">
+      <header>
+        <div className="app-bar">
+          <div className="app-header">
+          <h1>📝️ To Do List</h1>
+          </div>
+        </div>
+      </header>
+
+      <div className="main">
+        <TaskForm />
+
+        <ul className="tasks">
+          {tasks.map(task => (
+            <Task
+              key={task._id}
+              task={task}
+              onCheckboxClick={toggleChecked}
+              onDeleteClick={deleteTask}
+            />
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
